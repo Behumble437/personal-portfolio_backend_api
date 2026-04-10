76bf1d271da213aa0717f5b65ce47d727f267903
HXiong <asd475885437@gmail.com>
2026-04-10 02:20:30 -0400

    remove unnecessary file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,6 @@ const auth = require("../middleware/authMiddleware");
 router.post("/signup", controller.signup);
 router.post("/signin", controller.signin);
 
-router.post("/", controller.signup);
 router.get("/", controller.getUsers);
 router.get("/:id", controller.getUserById);
 router.put("/:id", auth, controller.updateUser);
